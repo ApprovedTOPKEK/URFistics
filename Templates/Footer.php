f53f42b0882ca15c6823ce5095b17cde332521a1
Voltier <nigelmartin.baldacchino@gmail.com>
2015-04-17 17:45:52 +0200

    Added copyright content  to footer

--- Templates/Footer.php
+++ Templates/Footer.php
@@ -1,5 +1,5 @@
 </div>
 <div style="clear: both;" class="push"></div>
-<div id="footer"><span id="footertext">Created for the Riot API Challenge. Copyrights 2015</span></div>
+<div id="footer"><span id="footertext">Created for the Riot API Challenge. Copyrights 2015</br></br>&copy; Background from Riot Games</span></div>
 </body>
 </html>
